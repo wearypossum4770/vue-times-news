da8d7a72560eb887a904e7151a79e05f5d97b5cd
stephen smith ecs <wearypossum4770@yahoo.com>
2021-11-08 12:46:29 -0500

    sync

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000
 express()  
 .use(favicon(__dirname + '/dist/favicon.ico'))
-.use(express.static(path.join(__dirname, 'public')))  
+.use(express.static(path.join(__dirname, 'dist')))  
 .get('/', (req, res) => res.render('dist/index.html'))
 .get('/ping', function (req, res) {
   return res.send('pong');
