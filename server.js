ac1570fa547071608cf9dd7fec1c6df443d09c91
stephen smith ecs <wearypossum4770@yahoo.com>
2021-11-08 12:35:29 -0500

    sync

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(favicon(__dirname + '/dist/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-app.listen(port);
+const PORT = process.env.PORT || 5000
+express()  
+.use(favicon(__dirname + '/dist/favicon.ico'))
+.use(express.static(path.join(__dirname, 'public')))  
+.get('/', (req, res) => res.render('dist/index.html'))
+.allget('/ping', function (req, res) {
+  return res.send('pong');
+ })
+ .listen(PORT, () => console.log(`Listening on ${ PORT }`))
