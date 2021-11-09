91c2e35f99c330b9953e866e12ba4f33b6babec6
Sara Mansouri <smansouri@c02xm2ysjhd2.myfiosgateway.com>
2021-11-09 14:02:43 -0500

    newslis is imported

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div >
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <NewsList />
+
   </div>
+  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import NewsList from './components/news/NewsList.vue'
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    NewsList
+
+  },
 }
 </script>
 
