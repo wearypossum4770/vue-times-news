a4a0e95e23eb340fe4ec8d6ecc5d90de270e67a7
stephen smith ecs <wearypossum4770@yahoo.com>
2021-11-09 22:20:33 -0500

    sync

--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -1,7 +1,7 @@
 <template> 
-<div> 
-  
-
+<div > 
+  <div id="pointer">Breaking News</div>
+<div>{{ news.title }}</div>
 </div> 
 </template> 
  
@@ -14,7 +14,9 @@ export default {
 name: 'NewsList', 
 
 data(){
-    return{}
+    return{
+        news: data[0],
+    }
     
 },
 
@@ -29,4 +31,38 @@ created(){
 
 
 <style scoped> 
+
+#pointer {
+  width: 200px;
+  height: 40px;
+  position: relative;
+  background: red;
+  text-align: center;
+  font-weight: bold;
+  display: flex;
+  justify-content: center; /* align horizontal */
+  align-items: center; /* align vertical */
+}
+#pointer:after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 0;
+  height: 0;
+  border-left: 20px solid white;
+  border-top: 20px solid transparent;
+  border-bottom: 20px solid transparent;
+}
+#pointer:before {
+  content: "";
+  position: absolute;
+  right: -20px;
+  bottom: 0;
+  width: 0;
+  height: 0;
+  border-left: 20px solid red;
+  border-top: 20px solid transparent;
+  border-bottom: 20px solid transparent;
+}
 </style> 
