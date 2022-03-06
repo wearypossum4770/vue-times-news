da02ccba7a68003d275e8367daef8cf568747f10
stephen smith ecs <wearypossum4770@yahoo.com>
2022-03-05 22:40:34 -0500

    getting team prepared

---
+++ tests/twoSum.js
@@ -0,0 +1,18 @@
+const twoSum = (nums, target) => {
+// 	let previousValues = new Map()
+// return previousValues
+// let currentValue, neededValue;
+// 	for (let index=0; index< nums.length; index++){
+//         currentValue = nums[index]
+// 		neededValue = target - nums[index]
+// 		if (previousValues.has(neededValue)){
+// 			return [previousValues.get(neededValue), index]
+// 		}
+// 		else {
+// 			previousValues.set(nums[index], index)
+// 		}
+// 	}
+// return []
+}
+
+export default twoSum;
