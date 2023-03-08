b3807458032234b3213b8a9a7f431741b3ff425d
Dev Steve <wearypossum4770@yahoo.com>
2023-03-08 07:11:25 -0500

    chore(refactor): combine vite and vitest config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    root: fileURLToPath(new URL('./', import.meta.url))
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
