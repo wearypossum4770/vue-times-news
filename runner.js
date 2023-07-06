741dc4982a68e9b47c41c86ad9b470cc38061850
wearypossum4770 <wearypossum4770@yahoo.com>
2023-07-06 14:37:46 -0400

    merge

--- runner.js
+++ runner.js
@@ -1,1 +1,14 @@
 const build = { Debug:"debug",Nightly:"nightly",Prod:"prod" }
+
+
+
+  const details = {
+    dateRecieved: Date.now(),
+    dateExpired: sampleExpiration(),
+    emailAddress,
+    channel: "SMS",
+    success: true,
+    message: "OTP sent to user",
+    otpId: otpValue,
+  };
+
